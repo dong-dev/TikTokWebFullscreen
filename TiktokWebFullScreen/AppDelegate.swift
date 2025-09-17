4b6b7c701600b265c3c2360e8001973cbc8cd67c
phuongdong <phuongdong@phuongdongs-Mac-Pro.local>
2025-09-17 20:49:08 +0700

    first commit

---
+++ TiktokWebFullScreen/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TiktokWebFullscreen
+//
+//  Created by Phương Đông on 15/9/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ TiktokWebFullScreenTests/TiktokWebFullScreenTests.swift
@@ -0,0 +1,17 @@
+//
+//  TiktokWebFullscreenTests.swift
+//  TiktokWebFullscreenTests
+//
+//  Created by Phương Đông on 15/9/25.
+//
+
+import Testing
+@testable import TiktokWebFullscreen
+
+struct TiktokWebFullscreenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
